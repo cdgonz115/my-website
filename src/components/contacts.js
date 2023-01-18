af4610586c0d93b9b220dfb20bd95304b6845aea
Christian Gonzalez <christiandash@hotmail.com>
2023-01-18 00:17:43 -0700

    Updated Contacts Styling
    Changes:
    -Changed the styling of the contacts component from js objects to a .css file.
    
    To Do:
    -Fix the issue with the footer not extending to the bottom
    -Continue moving the styles from js objects to .css files

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,72 +1,32 @@
 import React from 'react';
 
-const contactsFooterStyles = 
-{
-    padding: '2em',
-    fontSize: '75%',
-    fontStyle: 'italic',
-    textAlign: 'center',
-    marginTop: '50px',
-    fontFamily: "Georgia, 'Times New Roman', Times, serif",
-    backgroundColor: '#7a8fa7',
-    flex: '1',
-    maxHeight: '20%'
-}
-
-const contactsH3Styles = 
-{
-    textAlign: 'center',
-    padding: '30px',
-    fontSize: '1.3em',
-    letterSpacing: '.1em',
-    lineHeight: '2em',
-    fontFamily: "'Comfortaa', Arial, Helvetica, sans-serif",
-    fontWeight: 'bold',
-}
-const contactsDivStyles = {
-    display: 'flex',
-    overflowX: 'auto',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginTop: '50px',
-    marginBottom: '50px'
-}
-
-const contactIconDivStyles = {
-    borderRadius: '10px',
-    marginLeft: 'auto',
-    marginRight: 'auto'
-}
-
-const contactIconImgStyles = {
-    maxWidth: '50px'
-}
+import './contacts.css';
 
 export default function Contacts(){
 
 return (
-        <div style = {contactsFooterStyles}>
-            <h3 style = {contactsH3Styles}>
+        <div className = 'contactsFooter'>
+            <h3>
                 Socials
             </h3>
-            <div style = {contactsDivStyles}>
-                <div style = {contactIconDivStyles}>
+            <div className = 'contacts'>
+                <div className = 'contactsIcon'>
                     <a href="https://www.linkedin.com/in/christian-gonzalez-a297211b6" target="_blank">
-                        <img style = {contactIconImgStyles} src="media/Contacts/linkedin.png"/>
+                        <img  src="media/Contacts/linkedin.png" alt = 'linkedIn' />
                     </a>
                 </div>
-                <div style = {contactIconDivStyles}>
+                <div className = 'contactsIcon'>
                     <a href="https://github.com/cdgonz115" target="_blank">
-                        <img style = {contactIconImgStyles} src="media/Contacts/github.png"/>
+                        <img  src="media/Contacts/github.png" alt = 'github'/>
                     </a>
                 </div>
-                <div style = {contactIconDivStyles}>
+                <div className = 'contactsIcon'>
                     <a href="https://cg115.itch.io/" target="_blank">
-                        <img style = {contactIconImgStyles} src="media/Contacts/itch-io.png"/>
+                        <img src="media/Contacts/itch-io.png" alt = 'itch-io'/>
                     </a>
                 </div>
             </div>
-            Copyright &copy; 2021 Christian Gonzalez
+            Copyright &copy; 2023 Christian Gonzalez
             <br/>
             <a href="mailto:christiandgonzalez115@gmail.com">Christian Gonzalez</a> &nbsp; 
         </div>
