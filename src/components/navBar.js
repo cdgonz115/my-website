ba81d3ccb175a435eebe28923014d904005f9079
Christian Gonzalez <christiandash@hotmail.com>
2023-01-18 17:25:23 -0700

    navBar Styling
    Changes:
    -The navBar component now uses the .css file for styling and behaves correctly when viewed on small screens
    
    To Do:
    -Fix the styling on the contribution component to ensure the video component resizes appropriately to the size of the window

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,43 +3,7 @@ import {useDispatch } from 'react-redux';
 import {projects} from '../data/projectsData.js';
 import {changeProject} from '../features/contribution/contributionSlice.js';
 
-export const nav = 
-{
-    marginTop: '0px',
-    padding: '0px',
-    marginBottom: '30px',
-    fontWeight: 'bold',
-    fontSize: '120%',
-    textAlign: 'center',
-}
-
-export const navULWide = {
-    listStyleType: 'none',
-    paddingLeft: '0px',
-    fontSize: '1.2em',
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'nowrap',
-    justifyContent: 'space-around',
-    margin: '15px',   
-}
-
-export const navUL = {
-    listStyleType: 'none',
-    paddingLeft: '0px',
-    fontSize: '1.2em',
-    margin: '10px',
-}
-
-export const navA = 
-{
-    cursor: 'pointer',
-    textDecoration: 'none',
-    fontFamily:"'Righteous', Arial, Helvetica, sans-serif",
-    fontSize: '28px',
-    color: '#DC0128',
-    width: '100%'
-}
+import './navBar.css';
 
 export default function NavBar(){
     const dispatch = useDispatch();
@@ -48,12 +12,12 @@ export default function NavBar(){
         dispatch(changeProject(e.target.name));
     }
     return (
-        <div>
-            <nav style = {nav}>
-                <ul style = {navULWide}>
+        <div className='navBarWrapper'>
+            <nav >
+                <ul >
                     {projects.map((project) => (
                     <li key = {project.key}>
-                        <a onClick={handleClick} style = {navA} name = {project.key}>{project.title}</a>
+                        <a onClick={handleClick} name = {project.key}>{project.title}</a>
                     </li>))}
                 </ul>
             </nav>
