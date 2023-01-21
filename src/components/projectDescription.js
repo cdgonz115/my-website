cbf4f485760441c14289b311bf0f069cb5469fc7
Christian Gonzalez <christiandash@hotmail.com>
2023-01-21 02:23:53 -0700

    project description styling
    Changes:
    -Moved the styling of the projectDescription to its own .css file
    
    To Do:
    -Continue moving the styling of components to their own .css files

--- src/components/projectDescription.js
+++ src/components/projectDescription.js
@@ -2,43 +2,7 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 import {selectProject } from '../features/contribution/contributionSlice';
 import {descriptions } from '../data/projectsData';
-
-const projectSummaryH2Style = {
-    marginTop: '40px',
-    color: '#DC0128',
-    fontSize: '40px',
-    textAlign: 'center',
-    fontFamily: "'Comfortaa', Arial, Helvetica, sans-serif"
-}
-
-const projectSummaryPStyle = {
-    textAlign: 'center',
-    padding: '30px',
-    fontSize: '1.3em',
-    letterSpacing: '.1em',
-    lineHeight: '2em',
-    fontFamily: "'Comfortaa', Arial, Helvetica, sans-serif",
-    fontWeight: 'bold'
-}
-
-const projectLinkDivStyle = {
-    display: 'flex',
-    flexDirection: 'column'
-}
-const projectLinkH2Style = {
-    marginTop: '40px',
-    color: '#DC0128',
-    fontSize: '40px',
-    textAlign: 'center',
-    fontFamily: "'Comfortaa', Arial, Helvetica, sans-serif"
-}
-const projectLinkImgStyle = {
-    width: '80%',
-    borderRadius: '10px',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-}
-const projectLinkAStyle = {textDecoration: 'none'}
+import './projectDescription.css'
 
 export default function ProjectDescription(){
 
@@ -48,19 +12,20 @@ export default function ProjectDescription(){
 
     return (
         <div>
-             <div >
-                <h2 style = {projectSummaryH2Style}>
+             <div className='projectSummary'>
+                <h2>
                     {title}
                 </h2>
-                <p style = {projectSummaryPStyle}>
+                <p>
                     {projectSummary}
                 </p>
             </div>
-            {link!==undefined && (<div style = {projectLinkDivStyle}>
-                <a style = {projectLinkAStyle}href = {link} target="_blank">
-                    <h2 style = {projectLinkH2Style}>Download Here</h2>
+            {link!==undefined && (
+                <div className='projectLink'>
+                <a href = {link} target="_blank">
+                    <h2 >Download Here</h2>
                 </a>
-                <img style = {projectLinkImgStyle} src = {media} />
+                <img src = {media} />
             </div>)}
         </div>
     );
