81e90be7d1e9747c63741cb841cfb27388b2205d
Christian Gonzalez <christiandash@hotmail.com>
2023-01-21 02:52:13 -0700

    App Component Styling Updated
    Changes:
    -Updated the .css file for the app component
    
    To Do:
    -Fix bug that makes text go offscreen on the chronodin first contribution

--- src/App.js
+++ src/App.js
@@ -7,16 +7,15 @@ import './App.css';
 
 function App() {
   return (
-    <div style = {{height: '100%', display: 'flex', flexDirection: 'column'}}>
-      <header style = {{size: '30px',minWidth: '300px',padding: '0',}}className="App-header">
-      <a style = {{textDecoration: 'none'}}href="index.html">
-        <h1 style = {{margin: '0',fontFamily: "'Righteous', Arial, Helvetica, sans-serif",backgroundColor: '#0B0B4A',
-        color: '#DC0128',fontWeight: 'bold',fontSize: '60px',textAlign: 'center'}}>Christian Gonzalez</h1>
+    <div className='appWrapper '>
+      <header>
+      <a href="index.html">
+        <h1>Christian Gonzalez</h1>
       </a>
       <NavBar />
       </header>
       <Project />
-    <Contacts />
+      <Contacts />
     </div>
   );
 }
